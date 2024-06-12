2f4b895deb77808e0230447042a83fcc04928f98
kiraisnub <vrishinrajpurohit@gmail.com>
2024-06-12 15:57:24 +0530

    first commit

---
+++ newspaper_project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include # new
+from django.views.generic.base import TemplateView # new
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls')), # new
+    path('articles/', include('articles.urls')), # new
+    path('',include('pages.urls')),
+]
